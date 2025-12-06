8fafb396f7de19853e90eb26713fd138ff03b1aa
Akshay Chauhan <chauhanakshay395@gmail.com>
2025-12-06 10:01:59 +0530

    Added modelMapper

---
+++ src/main/java/com/akshay/CRUD/config/MapperConfig.java
@@ -0,0 +1,12 @@
+package com.akshay.CRUD.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public ModelMapper getModelMapper(){return new ModelMapper();}
+}
