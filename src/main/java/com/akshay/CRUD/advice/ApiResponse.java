928f45b49fce5c50523c8967012a3c8f400928ad
Akshay Chauhan <chauhanakshay395@gmail.com>
2025-12-06 10:41:52 +0530

    Added ApiError and ApiResponse class in advices

---
+++ src/main/java/com/akshay/CRUD/advice/ApiResponse.java
@@ -0,0 +1,25 @@
+package com.akshay.CRUD.advice;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiResponse<T> {
+
+    private LocalDateTime timestamp;
+    private T data;
+    private ApiError error;
+
+    public ApiResponse() {this.timestamp = LocalDateTime.now();}
+
+    public ApiResponse(T data){
+        this(); // calls default constructor
+        this.data = data;
+    }
+
+    public ApiResponse(ApiError error){
+        this();
+        this.error = error;
+    }
+}
